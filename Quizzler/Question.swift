579b7926d35e290d355aff3f0a52505f354a5ed5
psx95 <pranav.ps95@hotmail.com>
2019-01-26 12:02:16 +0530

    Adds the QuestionBank class, Fixed typo in Question class

--- Quizzler/Question.swift
+++ Quizzler/Question.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Pranav Sharma on 24/01/19.
 //  Copyright © 2019 London App Brewery. All rights reserved.
-//
+// This is Model in the MVC Pattern
 
 import Foundation
 
-class Quesiton {
+class Question {
     
     let questionText: String
     let answer: Bool
